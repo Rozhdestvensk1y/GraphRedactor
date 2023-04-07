c0f4c6e1a8dbd6b9e036a190fae7f9f896845d2e
Артём Рождественский <arojdestvenskiy@timepath.ru>
2023-04-08 01:51:25 +0500

    test

---
+++ GraphRedactor/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphRedactor
+{
+    internal class Class1
+    {
+    }
+}
